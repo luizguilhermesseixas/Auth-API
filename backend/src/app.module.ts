9910f49fa8455555a300a7ba39f2ae499f1a2bc8
Luiz Guilherme Seixas <guilhermesschiavon@gmail.com>
2025-10-30 02:28:35 -0400

    feat: install passport and create auth module and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConfigModule } from '@nestjs/config/dist/config.module.js';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
