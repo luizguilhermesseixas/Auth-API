e18fbdd978e2bbce5fbf8a53d5ad3d615948d919
Luiz Guilherme Seixas <guilhermesschiavon@gmail.com>
2025-10-30 20:43:35 -0400

    feat: create user repository module, service, controller, dto and reposirory

---
+++ backend/src/users/dtos/create-user.dto.ts
@@ -0,0 +1,56 @@
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class CreateAddressDto {
+  @IsString()
+  @MaxLength(255)
+  street!: string;
+
+  @IsString()
+  @MaxLength(100)
+  city!: string;
+
+  @IsString()
+  @MaxLength(100)
+  state!: string;
+
+  @IsString()
+  @MaxLength(20)
+  zipCode!: string;
+
+  @IsString()
+  @MaxLength(100)
+  country!: string;
+}
+
+export class CreateUserDto {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(100)
+  password!: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
+  firstName!: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
+  lastName!: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CreateAddressDto)
+  address?: CreateAddressDto;
+}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { UsersService } from './users.service.js';
 
 @Module({
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
