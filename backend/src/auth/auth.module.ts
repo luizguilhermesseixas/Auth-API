dd3f4956cefdc69295393e9ba13a1eb7047ccf77
Luiz Guilherme Seixas <guilhermesschiavon@gmail.com>
2025-11-11 01:51:45 -0500

    feat: create passport auth local strategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
+import { UsersModule } from 'src/users/users.module.js';
 
 @Module({
+  imports: [UsersModule],
   providers: [AuthService],
 })
 export class AuthModule {}
