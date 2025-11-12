de8712d0740155c223c00d5327cf45bf5ea78d3f
Luiz Guilherme Seixas <guilhermesschiavon@gmail.com>
2025-11-11 23:33:58 -0500

    refactor: change localstrategy params name

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { UsersModule } from 'src/users/users.module.js';
+import { LocalStrategy } from './strategies/local.strategy.js';
 
 @Module({
   imports: [UsersModule],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
